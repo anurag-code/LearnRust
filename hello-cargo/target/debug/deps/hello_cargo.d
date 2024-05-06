db68e5119aa3e173833e3e57435f1a6e01153311
ANURAG PANDEY <anunitb@gmail.com>
2024-05-07 00:29:46 +1000

    hello cargo

---
+++ hello-cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\coding\LearnRust\hello-cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\coding\LearnRust\hello-cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+C:\coding\LearnRust\hello-cargo\target\debug\hello-cargo.exe: C:\coding\LearnRust\hello-cargo\src\main.rs
